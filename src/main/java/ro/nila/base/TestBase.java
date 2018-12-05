7e439c235c75e7cc57466889e24e72cd4b519e3c
Alin Popa <29579991+PopaAlinRadu@users.noreply.github.com>
2018-12-05 10:59:19 +0200

    started to implement ExtentReports

--- src/main/java/ro/nila/base/TestBase.java
+++ src/main/java/ro/nila/base/TestBase.java
@@ -1,7 +1,12 @@
 package ro.nila.base;
 
-import org.testng.annotations.*;
-import ro.nila.business.Account;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import ro.nila.utilities.PropertiesManager;
 import ro.nila.utilities.configuration.ConfigManager;
 import ro.nila.utilities.configuration.TxtManager;
@@ -10,12 +15,13 @@ import ro.nila.utilities.configuration.WebDriverManager;
 
 import java.io.IOException;
 
-import static ro.nila.utilities.PropertiesManager.*;
+import static ro.nila.utilities.PropertiesManager.closeWebDriver;
+import static ro.nila.utilities.PropertiesManager.quitWebDriver;
 
-public abstract class TestBase {
+public abstract class TestBase implements ITestListener {
 
 
-    public static PropertiesManager uiElementsManager, txtManager, configManager, webDriverManager;
+    public static PropertiesManager uiElementsManager, txtManager, configManager, webDriverManager, report, test;
 
     static{
         configManager = new ConfigManager();
@@ -25,10 +31,8 @@ public abstract class TestBase {
     }
 
     // Needs to contain:
-
     // Initialize Configuration - (config.properties) + (ui.properties) + (WebDriver) + (ExtentReports) + (ExtentTest)
     // Annotation from TestNG
-
     @BeforeSuite
     public void beforeSuite() throws IOException {
         uiElementsManager.loadConfiguration();
@@ -51,4 +55,39 @@ public abstract class TestBase {
         quitWebDriver();
     }
 
+    // Methods from ITestListener interface
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.println("onTestStart ");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("onTestSucces");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("onTestFailure");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println("onTestSkipped");
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        System.out.println("onTestFailedButWithinSuccessPercentage");
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        System.out.println("onStart");
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        System.out.println("onFinish");
+    }
 }
--- src/main/java/ro/nila/listeners/CustomListeners.java
+++ src/main/java/ro/nila/listeners/CustomListeners.java
@@ -1,43 +1,10 @@
 package ro.nila.listeners;
 
 import org.testng.ITestContext;
-import org.testng.ITestListener;
 import org.testng.ITestResult;
+import ro.nila.base.TestBase;
 
-public class CustomListeners implements ITestListener {
+public class CustomListeners {
 
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
-        System.out.println("onTestStart ");
-    }
 
-    @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-        System.out.println("onTestSucces");
-    }
-
-    @Override
-    public void onTestFailure(ITestResult iTestResult) {
-        System.out.println("onTestFailure");
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-        System.out.println("onTestSkipped");
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-        System.out.println("onTestFailedButWithinSuccessPercentage");
-    }
-
-    @Override
-    public void onStart(ITestContext iTestContext) {
-        System.out.println("onStart");
-    }
-
-    @Override
-    public void onFinish(ITestContext iTestContext) {
-        System.out.println("onFinish");
-    }
 }
